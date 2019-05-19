24a52cacd16248cfe184c01c1248be2719164711
Pandaaaa906 <ye.pandaaaa906@gmail.com>
2019-05-19 18:08:39 +0800

    20190519 shrink the screen shot box to accelerate

--- main.py
+++ main.py
@@ -16,8 +16,8 @@ tmp_img_fp = "screenshots/tmp_{}.png"
 
 threshold = 50
 
-l_indexes = ([242, 192, 142, 92, 42], [270, ] * 5)
-r_indexes = ([242, 192, 142, 92, 42], [330, ] * 5)
+l_indexes = ([242, 192, 142, 92, 42], [0, ] * 5)
+r_indexes = ([242, 192, 142, 92, 42], [59, ] * 5)
 
 
 def func(arr):
@@ -34,8 +34,22 @@ def draw_detect_point(img, fp):
     img.save(fp)
 
 
+def crop_box(box: dict) -> dict:
+    x, y, width, height = box.values()
+    mid = width / 2
+    x = mid - 30
+    width = 60
+    result = {
+        'x': x,
+        'y': y,
+        'width': width,
+        'height': 245,
+    }
+    return result
+
+
 async def _main(url):
-    page = await new_browser_page(headless=False)
+    page = await new_browser_page(headless=True)
 
     await page.goto(url)
     btn_start = await page.waitForXPath('//div[@class="icon_play"]')
@@ -43,33 +57,38 @@ async def _main(url):
 
     content = await page.waitForXPath('//div[@class="page_content"]')
     box = await content.boundingBox()
+    box = crop_box(box)
 
     btn_left = await page.waitForXPath('//div[@id="button_left"]')
     btn_right = await page.waitForXPath('//div[@id="button_right"]')
 
-    for count in range(200):
+    await page.waitFor('.in_game')
+    print("Playing...")
+    while await page.querySelectorAll('.in_game'):
         b_data = await page.screenshot(clip=box)
         img = Image.open(BytesIO(b_data))
 
         arr = np.array(img)
 
-        l_arr = (norm(arr[l_indexes] - branch_color, axis=1) - threshold > 0)
-        r_arr = (norm(arr[r_indexes] - branch_color, axis=1) - threshold > 0)
-
-        l_count = func(l_arr)
-        r_count = func(r_arr)
+        l_count = func(norm(arr[l_indexes] - branch_color, axis=1) - threshold > 0)
+        r_count = func(norm(arr[r_indexes] - branch_color, axis=1) - threshold > 0)
 
         # draw_detect_point(img, tmp_img_fp.format(count))
 
         if l_count > r_count:
-            for _ in range(l_count-1):
+            for _ in range(l_count - 1):
                 await btn_left.click()
         else:
-            for _ in range(r_count-1):
+            for _ in range(r_count - 1):
                 await btn_right.click()
+    score = await (await (await page.waitForXPath('//div[@id="score_value"]/text()')).getProperty("textContent")).jsonValue()
+    print(f"Your Score This Time: {score}")
+    await page.browser.close()
 
 
 if __name__ == '__main__':
-    url = input("Input your game link: ")
+    url = "https://tbot.xyz/lumber/#eyJ1Ijo3MTgyMjI2NDIsIm4iOiJQYW5kYWFhYSBZaXAiLCJnIjoiTHVtYmVySmFjayIsImNpIjoiLTI0MDI5MTYyMTk1MTA3NTc5OTUiLCJpIjoiQlFBQUFNaklBUUFtbERLN3NXU19Ra3l1TWJvIn0xNzEwMmM4NzQ3YmIwYzQzMDdkZTE0YmNhNGM3NzkzYw==&tgShareScoreUrl=tg%3A%2F%2Fshare_game_score%3Fhash%3D_MPgn1QCmICtyYbxbIteLdDhQz48QWyZKcS6lElUgkc"
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(_main(url))
+    while True:
+        # url = input("Input your game link: ")
+        loop.run_until_complete(_main(url))
