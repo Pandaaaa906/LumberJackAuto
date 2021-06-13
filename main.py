f84b380803bf0cbdea95051061f61dc64ac6c6b7
Pandaaaa906 <ye.pandaaaa906@gmail.com>
2021-06-13 20:51:11 +0800

    improve detection func

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import asyncio
 from io import BytesIO
 from itertools import chain
+from os import getenv
 
 from PIL import Image
 from PIL.ImageDraw import Draw
@@ -9,28 +10,35 @@ import numpy as np
 from loguru import logger
 from more_itertools import first
 from numpy.linalg import norm
+from pyppeteer import launch
+from pyppeteer_stealth import stealth
 
-from browser import new_browser_page
 
-branch_color = np.array([161, 116, 56, 255])
+HEADLESS = getenv('HEADLESS', True)
+BROWSER_DATA_DIR = getenv('BROWSER_DATA_DIR', './tmp')
 
-tmp_img_fp = "screenshots/tmp_{}.png"
-
-threshold = 50
-
+BRANCH_COLOR = np.array([161, 116, 56, 255])
+THRESHOLD = 50
 N_LOOK_AHEAD = 5
 X_LEFT_BRANCH = 0
 X_RIGHT_BRANCH = 59
 Y_START = 42
 Y_GAP = 50
+WIDTH = 60
+HEIGHT = Y_START + Y_GAP * (N_LOOK_AHEAD-1) + 3
 Y_INDEXES = tuple(reversed(tuple(Y_START + Y_GAP * i for i in range(N_LOOK_AHEAD))))
 
 l_indexes = (Y_INDEXES, [X_LEFT_BRANCH, ] * N_LOOK_AHEAD)
 r_indexes = (Y_INDEXES, [X_RIGHT_BRANCH, ] * N_LOOK_AHEAD)
 
+default_args = [
+    '--no-sandbox',
+]
+tmp_img_fp = "screenshots/tmp_{}.png"
+
 
 def nearest_branch(arr, indexes):
-    return first(np.where((norm(arr[indexes] - branch_color, axis=1) > threshold) == False)[0], N_LOOK_AHEAD)
+    return first(np.where((norm(arr[indexes] - BRANCH_COLOR, axis=1) > THRESHOLD) == False)[0], N_LOOK_AHEAD)
 
 
 def draw_detect_point(img, fp):
@@ -41,20 +49,20 @@ def draw_detect_point(img, fp):
 
 
 def crop_box(box: dict) -> dict:
-    x, y, width, height = box.values()
-    mid = width / 2
-    x = mid - 30
-    width = 60
+    x, _, width, _ = box.values()
     return {
-        'x': x,
-        'y': y,
-        'width': width,
-        'height': 245,
+        'x': (width - WIDTH) / 2,
+        'y': 0,
+        'width': WIDTH,
+        'height': HEIGHT,
     }
 
 
-async def _main(url: str):
-    page = await new_browser_page(headless=True)
+async def async_play(url: str):
+    browser = await launch(headless=HEADLESS, userDataDir=BROWSER_DATA_DIR, ignoreHTTPSErrors=True, args=default_args)
+    page = await browser.newPage()
+    await stealth(page)
+    await page.setViewport({'width': 600, 'height': 600})
 
     await page.goto(url)
     btn_start = await page.waitForXPath('//div[@class="icon_play"]')
@@ -68,16 +76,14 @@ async def _main(url: str):
 
     await page.waitFor('.in_game')
     logger.info("Playing...")
+    count = 0
     while await page.querySelectorAll('.in_game'):
+        count += 1
         b_data = await page.screenshot(clip=box)
         img = Image.open(BytesIO(b_data))
-
         arr = np.array(img)
-
         l_count, r_count = nearest_branch(arr, l_indexes), nearest_branch(arr, r_indexes)
-
-        # draw_detect_point(img, tmp_img_fp.format(count))
-
+        draw_detect_point(img, tmp_img_fp.format(count))
         if l_count > r_count:
             for _ in range(l_count - 1):
                 await btn_left.click()
@@ -90,8 +96,12 @@ async def _main(url: str):
     await page.browser.close()
 
 
+def main(url: str):
+    while True:
+        asyncio.run(async_play(url))
+
+
 if __name__ == '__main__':
     url = "https://tbot.xyz/lumber/#eyJ1Ijo3MTgyMjI2NDIsIm4iOiJQYW5kYWFhYSBZaXAiLCJnIjoiTHVtYmVySmFjayIsImNpIjoiNzg1NTYxMTg0NDEzMzgyNzgyOCIsImkiOiJCUUFBQUJnUUV3RGxQRTNCR3oyVkgzRnNGU1EifWFiODNlOTZjMmI2NjAzYWVkNjlkZTBkNDdjMTQyZGI5?tgShareScoreUrl=tgb%3A%2F%2Fshare_game_score%3Fhash%3DxrwtNOUxByeJDXVIyEou"
     # url = input("Input your game link: ")
-    while True:
-        asyncio.run(_main(url))
+    main(url)
